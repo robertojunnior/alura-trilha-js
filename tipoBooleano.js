3716f6629c14f5d01cee7759195a441a0bfe1030
roberjunnior <roberjunior.dev@gmail.com>
2023-12-09 06:44:47 -0300

    update tipo boolean

--- tipoBooleano.js
+++ tipoBooleano.js
@@ -1,8 +1,16 @@
 // boolean
-// true >> verdadeiro
-// false >> falso
+// true -> verdadeiro
+// false -> falso
 
 const primeiroNumero = 5;
 const segundoNumero = 10;
+let cadastroAtivado = true;
 
 console.log(primeiroNumero === segundoNumero);
+
+//-------------------------------------------
+
+const texto1 = "Alura";
+const texto2 = "alura";
+
+console.log(texto1 === texto2);
