01419fa94a717f8a0319620ed7bea6b5fa3d4269
roberjunnior <roberjunior.dev@gmail.com>
2023-12-09 05:11:17 -0300

    update tipo string

---
+++ tipoString.js
@@ -0,0 +1,14 @@
+const texto1 = "Olá, mundo!";
+const texto2 = "Olá, mundo!";
+const senha = "senhaSegura1234@!";
+const stringDeNumeros = "12345678910";
+
+const aspasSimples = 'O Leo disse: "oi!"';
+const citacao = " Meu nome é ";
+const meuNome = "Leonardo";
+
+// concatenação (+)
+
+console.log(citacao + meuNome);
+
+// template string ou template literal
