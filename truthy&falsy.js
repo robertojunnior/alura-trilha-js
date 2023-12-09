5503d7cdcd846c9b87609d0d56406df84251ea19
roberjunnior <roberjunior.dev@gmail.com>
2023-12-09 07:27:25 -0300

    update variaveis

---
+++ truthy&falsy.js
@@ -0,0 +1,22 @@
+// boolean
+
+const usuarioLogado = true;
+const contaPaga = false;
+
+// truthy ou falsy
+// 0 => false
+// 1 => true
+
+console.log(0 == false);
+console.log("" == false);
+console.log(1 == true);
+
+// null -> vazio
+let minhaVar;
+let varNull = null;
+
+let numero = 3;
+let texto = "Alura";
+
+console.log(minhaVar);
+console.log(varNull);
