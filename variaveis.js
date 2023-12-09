0f5a8f0870997bbba874600daf0bb8827a3a86e2
roberjunnior <roberjunior.dev@gmail.com>
2023-12-09 07:10:41 -0300

    update variaveis

---
+++ variaveis.js
@@ -0,0 +1,38 @@
+// var
+
+// var altura = 5;
+// var comprimento = 7;
+
+// area = altura * comprimento;
+// console.log(area);
+// var area;
+
+// let
+
+// let forma = "retangulo";
+// let altura = 5;
+// let comprimento = 7;
+// let area;
+
+// if (forma === "retangulo") {
+//   area = altura * comprimento;
+// } else {
+//   area = (altura * comprimento) / 2;
+// }
+
+// console.log(area);
+
+// const
+
+const forma = "triangulo";
+const altura = 5;
+const comprimento = 7;
+let area;
+
+if (forma === "quadrado") {
+  area = altura * comprimento;
+} else {
+  area = (altura * comprimento) / 2;
+}
+
+console.log(area);
