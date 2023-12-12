77860625e2065b8c8e06b31b3652abf13cba47e3
roberjunnior <roberjunior.dev@gmail.com>
2023-12-12 03:50:49 -0300

    update - Convertendo tipos

--- conversaoDeTipos.js
+++ conversaoDeTipos.js
@@ -1,1 +1,17 @@
-                              
+// tipo de dado
+// booleano
+
+// conversão implícita
+const numero = 456;
+const numeroString = "456";
+
+console.log(numero === numeroString); // verificou valor e tipo
+console.log(numero == numeroString); // verificou equivalência
+console.log(numero + numeroString); // concatenou
+
+// conversão explícita
+// Number() -N maiúsculo => string()
+console.log(numero + Number(numeroString));
+
+const numeroString2 = Number("123");
+console.log(numero + numeroString2);
