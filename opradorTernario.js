0f55c8dc7b1670975e39532c5266ccaee2dbaa82
roberjunnior <roberjunior.dev@gmail.com>
2023-12-14 04:46:23 -0300

    update - Operador ternário

---
+++ opradorTernario.js
@@ -0,0 +1,10 @@
+const idadeMinima = 18;
+let idadeCliente = 16;
+
+// if (idadeCliente >= idadeMinima) {
+//   console.log("cerveja");
+// } else {
+//   console.log("suco");
+// }
+
+console.log(idadeCliente >= idadeMinima ? "Cerveja" : "Suco");
