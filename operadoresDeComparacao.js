12b607b7e23f21805d2c89ddde37a427c3475f0b
roberjunnior <roberjunior.dev@gmail.com>
2023-12-14 04:28:58 -0300

    update - Operadores de Comparação

---
+++ operadoresDeComparacao.js
@@ -0,0 +1,12 @@
+// 2 iguais == para comparação sem tipo (Comparação implícita)
+
+const numero = 5;
+const texto = "5";
+
+console.log(numero == texto);
+
+// 3 iguais === para comparação com tipos (Comparação explícita)
+
+console.log(numero === texto);
+console.log(typeof numero);
+console.log(typeof texto);
