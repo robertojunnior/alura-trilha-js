a6fef3426ce561d797431301b57f751b5c80c24e
roberjunnior <roberjunior.dev@gmail.com>
2023-12-14 05:00:13 -0300

    update - template string

---
+++ templateString.js
@@ -0,0 +1,7 @@
+const nome = "Roberto";
+const sobrenome = "Junior";
+const idade = 2023 - 1981;
+const cidadeNascimento = "Santo André";
+const apresentacao = `Olá, meu nome é ${nome} ${sobrenome}, tenho ${idade} anos e nasci na cidade de ${cidadeNascimento} - SP.`;
+
+console.log(apresentacao);
