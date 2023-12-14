8b9e02e0ee83db4827217290be1a3732dec58a05
roberjunnior <roberjunior.dev@gmail.com>
2023-12-14 04:30:08 -0300

    update - Operadores de Comparação

--- operadoresDeComparacao.js
+++ operadoresDeComparacao.js
@@ -10,3 +10,7 @@ console.log(numero == texto);
 console.log(numero === texto);
 console.log(typeof numero);
 console.log(typeof texto);
+
+// Usar sempre os 3 iguais === como boas práticas converter de maneira explícita:
+Number();
+String();
