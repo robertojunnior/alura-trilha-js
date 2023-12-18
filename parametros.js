eaccc1e017d54d316e31742fdb0b7eecd2addf05
roberjunnior <roberjunior.dev@gmail.com>
2023-12-18 15:25:52 -0300

    update - parametros

--- parametros.js
+++ parametros.js
@@ -14,3 +14,14 @@ console.log(soma(12, 9));
 
 // usando os parâmetros recebidos das entradas de prompt
 alert(`A soma entre ${p1} e ${p2} é igual a ${soma(p1, p2)}.`);
+
+// parâmetros x funções
+
+function nomeSobrenome(nome, sobrenome) {
+  return `Muito prazer em lhe conhecer ${nome} ${sobrenome}, seja bem vindo!`;
+}
+
+// as variáveis de entrada podem ser declaradas antes ou depois da função, depende do projeto, o importante é executar após.
+let nome = prompt("Qual é o seu 1º nome?");
+let sobrenome = prompt("E agora o seu último nome?");
+alert(nomeSobrenome(nome, sobrenome));
