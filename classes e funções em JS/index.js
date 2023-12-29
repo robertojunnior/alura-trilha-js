3fdcd5a66b54b8bb0a089ffbb29cff9b200e8009
roberjunnior <roberjunior.dev@gmail.com>
2023-12-29 14:27:03 -0300

    update - classes

--- classes e funções em JS/index.js
+++ classes e funções em JS/index.js
@@ -1,5 +1,4 @@
-// criando objeto para utilização de classes
-
+// criando objeto para utilização de classes (Objeto literal)
 const livro = {
     nome: "React Native",
     editora: "Casa do Código",
@@ -11,3 +10,42 @@ const livro = {
 }
 
 livro.anunciar();
+
+// espaço no node
+console.log("*************************************************");
+
+// segundo exemplo, vou incrementar os prompts para entrada dos dados
+
+// Rodar no navegador
+
+// alert("Olá, para adicionar um novo Livro, responda as perguntas à seguir!")
+// let entradaNome = prompt("Qual o nome do novo Livro? ")
+// let entradaEditora = prompt("Qual o nome da Editora? ")
+// let entradaPaginas = prompt("Quantas páginas tem o novo Livro? ")
+
+
+// rodar no Node
+
+let entradaNome = "José e as cabras no nordeste"
+let entradaEditora = "Cabra da peste"
+let entradaPaginas = "185"
+
+const Livro = function (){
+    gNome = entradaNome,
+    gEditora = entradaEditora,
+    gPaginas = entradaPaginas
+
+    this.getNome = function () {
+        return gNome;
+    }
+    this.getEditora = function() {
+        return gEditora;
+    }
+    this.getPaginas = function(){
+        return gPaginas;
+    }
+}
+const novoLivro = new Livro(entradaNome, entradaEditora, entradaPaginas);
+
+console.log(novoLivro);
+console.log(`O novo livro adicionado foi ${novoLivro.getNome()}, da Editora ${novoLivro.getEditora()} e possui ${novoLivro.getPaginas()} páginas.`);
