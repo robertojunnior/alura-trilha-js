53b0096ef204aab35e6b060d6a432e66ebde0084
roberjunnior <roberjunior.dev@gmail.com>
2023-12-30 14:49:10 -0300

    update - push

---
+++ arrays, loops e callback/adicionar-nota.js
@@ -0,0 +1,7 @@
+// -------------- Adicionar um novo elemento no final do array (PUSH)
+const notas = [10, 6, 8];
+
+notas.push(7);
+const media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length;
+
+console.log(media);
--- arrays, loops e callback/media.js
+++ arrays, loops e callback/media.js
@@ -1,4 +1,4 @@
-// sem estrutura de repetição
+// sem estrutura de array
 // const nota1 = 10;
 // const nota2 = 6.5;
 // const nota3 = 8;
@@ -8,6 +8,7 @@
 // console.log(`A média das notas tiradas foi ${media} pontos.`);
 
 //-----------------------------------------------------------
+// Com estrutura de array
 
 const notas = [10, 6.5, 8, 7.5];
 const media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length;
