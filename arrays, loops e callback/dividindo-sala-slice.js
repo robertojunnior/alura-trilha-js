9e903e9cb994abbf7108164ea58d4e2139e0cd8b
roberjunnior <roberjunior.dev@gmail.com>
2023-12-31 02:33:32 -0300

    update - pop, push, slice

--- arrays, loops e callback/dividindo-sala-slice.js
+++ arrays, loops e callback/dividindo-sala-slice.js
@@ -20,10 +20,10 @@ const alunos = [
   "Daisy",
   "Camilo",
 ];
-// SLICE retorna os primeiros 10 elementos do array 'alunos', lembrando que ele vai do índice 0 até 0 10º ou seja, até o índice 9 "André";
+// SLICE retorna os primeiros 10 elementos do array 'alunos', lembrando que ele vai do índice 0 até o 10º ou seja, até o índice 9 "André", passando os parâmetros slice(0, 10)
 const sala1 = alunos.slice(0, alunos.length / 2);
 console.log(` A lista de alunos da Sala 1 é: ${sala1}.`);
 
-// se não colocarmos dois parâmetros no slice, ele segue até o final da lista
+// se não colocarmos dois parâmetros no slice, ele segue da metade até o final da lista
 const sala2 = alunos.slice(alunos.length / 2);
 console.log(` A lista de alunos da Sala 2 é: ${sala2}.`);
