509139a4332898a3f54675c87776f5d79296222a
roberjunnior <roberjunior.dev@gmail.com>
2023-12-31 02:36:31 -0300

    update - pop, push, slice

--- arrays, loops e callback/dividindo-sala-slice.js
+++ arrays, loops e callback/dividindo-sala-slice.js
@@ -27,3 +27,6 @@ console.log(` A lista de alunos da Sala 1 é: ${sala1}.`);
 // se não colocarmos dois parâmetros no slice, ele segue da metade até o final da lista
 const sala2 = alunos.slice(alunos.length / 2);
 console.log(` A lista de alunos da Sala 2 é: ${sala2}.`);
+
+// outro exemplo livre
+console.log(alunos.slice(3, 8)); // a contagem sempre para no índice anterior
