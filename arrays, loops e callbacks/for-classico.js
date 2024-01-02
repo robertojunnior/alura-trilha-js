699209603a2e955aa9cd9c8e2ef398ba1b0fa469
roberjunnior <roberjunior.dev@gmail.com>
2024-01-01 23:45:18 -0300

    update - for clássico

---
+++ arrays, loops e callbacks/for-classico.js
@@ -0,0 +1,10 @@
+const numeros = [100, 200, 300, 400, 500, 600];
+
+// a primeira expressão é executada apenas 1 vez
+// a segunda expressão é a condição da execução
+// a terceira expressão é o incremento ou decremento (ou outra operação) que será realizado após cada it
+
+//   declara      ; enquanto   ; incremente
+for (let indice = 0; indice < numeros.length; indice++) {
+  console.log(numeros[indice]);
+}
