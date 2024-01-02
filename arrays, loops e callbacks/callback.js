9e8cdfb99122197619ddfd5ac33e435c94d91186
roberjunnior <roberjunior.dev@gmail.com>
2024-01-02 17:40:30 -0300

    update - callbacks

--- arrays, loops e callbacks/callback.js
+++ arrays, loops e callbacks/callback.js
@@ -1,12 +1,24 @@
 const nomes = ["Roberto", "Jurema", "Gertrudes", "Filomena"];
 
 nomes.forEach(function(nome){
-    console.log(`Olá ${nome}!`);  // Olá Roberto!, Olá Jurema!, Olá Gertrudes", Olá Filomena!
+    console.log(`Olá ${nome}!`); 
+    // Olá Roberto!, Olá Jurema!, Olá Gertrudes", Olá Filomena!
 });
 
+//------------------------------------------------------------
 // Usando arrow function (função de seta) para simplificar a sintaxe:
 const nomes2 = ["Roberval", "Jucilene", "Jerônimo", "Filisteu"];
 
 nomes2.forEach((nome)=>{
-    console.log(`Olá ${nome}!`)
+    console.log(`Olá ${nome}!`);
+    // Olá Roberval! Olá Jucilene! Olá Jerônimo! Olá Filisteu! 
 });
+
+//----------------------------------------------------------
+const nomes3 = ["Rodrigo", "José", "Marcos", "Olavo"];
+
+function imprimeNome(nome) {
+    console.log(nome);
+}
+
+nomes3.forEach(imprimeNome);
