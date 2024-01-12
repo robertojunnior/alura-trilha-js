ceacef4668fa4367252cce653b9d7de40d5fcd1e
roberjunnior <roberjunior.dev@gmail.com>
2024-01-12 00:46:44 -0300

    update - readme

---
+++ arrays, loops e callbacks/boroca.js
@@ -0,0 +1,7 @@
+let notaSemestre = 8
+
+if (notaSemestre >= 8) {
+    console.log(`Sua nota é ${notaSemestre}, então C passou oreia seca!`);
+} else {
+    console.log(`Sua nota é ${notaSemestre}, então C se lascou!`);
+}
