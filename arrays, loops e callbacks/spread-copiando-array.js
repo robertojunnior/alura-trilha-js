65dcf3b215bb9742acc984dd232483ea95ac472f
roberjunnior <roberjunior.dev@gmail.com>
2024-01-20 04:24:58 -0300

    update - spread

--- arrays, loops e callbacks/spread-copiando-array.js
+++ arrays, loops e callbacks/spread-copiando-array.js
@@ -6,3 +6,14 @@ const novasNotas = ["add antes e depois do ...", ...notas, 10, 6, "teste"]; // s
 
 console.log(`As notas originais são: [${notas}]`);
 console.log(`As novas notas são: [${novasNotas}]`);
+
+//---------------------------
+
+const livros = ["Senhor do seu anel ", " As múmias de avalon"]
+
+const novosLivros = ["Papa é Pop ", ...livros, " Josué e os 7 camelos"]
+
+console.log(`A lista antiga dos livros é: ${livros}`);
+console.log(`A nova lista de livros é: ${novosLivros}.`);
+
+// usando o operador "..." Spread Operator, podemos copiar o array antigo e adicionar novos indices no novo array
