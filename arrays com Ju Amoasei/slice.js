461b5edcdb17d5838e584755efb3a94bd419a452
roberjunnior <roberjunior.dev@gmail.com>
2024-01-21 04:16:53 -0300

    update - slice

---
+++ arrays com Ju Amoasei/slice.js
@@ -0,0 +1,10 @@
+const listaEstudantes = ['Joao', 'Juliana', 'Ana', 'Caio', 'Lara',
+    'Marjorie', 'Guilherme', 'Aline', 'Fabiana', 'André', 'Carlos',
+    'Paulo', 'Bia', 'Vivian', 'Isabela', 'Vinícius', 'Renan', 'Renata',
+    'Daisy', 'Camilo'];
+
+
+const sala1 = listaEstudantes.slice([0], listaEstudantes.length / 2);
+const sala2 = listaEstudantes.slice(listaEstudantes.length / 2, [20]);
+console.log(`A sala 1 contém os alunos: ${sala1}`);
+console.log(`A sala 2 contém os alunos: ${sala2}`);
