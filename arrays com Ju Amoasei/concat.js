45ad5a303e0d1bba89e8545c918567b81365a9f4
roberjunnior <roberjunior.dev@gmail.com>
2024-01-22 02:24:07 -0300

    update - matriz

--- arrays com Ju Amoasei/concat.js
+++ arrays com Ju Amoasei/concat.js
@@ -3,3 +3,18 @@ const sala2 = ['Carlos', 'Paulo', 'Bia', 'Vivian', 'Isabela', 'Vinícius', 'Rena
 
 const salas = sala1.concat(sala2);
 console.log(salas);
+
+//-------------------------------
+const arrayOriginal = ["Maria", "Carlos", "Eduardo", "Samanta"]
+const arrayConcat = arrayOriginal.concat(["André", "Fernanda"], ["Ricardo", "Ana"], ["Marcelo", "Bia"])
+
+console.log(`Este é o array original: ${arrayOriginal}`)
+console.log(`Este é o array concat: ${arrayConcat}`)
+
+//-------------------------------
+
+const arrayOriginal1 = [50, 60, 70]
+const arrayConcat1 = arrayOriginal1.concat([80, [90, 100]])
+
+console.log(arrayConcat1)
+console.log(arrayOriginal1)
--- arrays com Ju Amoasei/matriz.js
+++ arrays com Ju Amoasei/matriz.js
@@ -1,18 +1,16 @@
 const alunos = ["João", "Juliana", "Caio", "Ana"];
-const medias = [10, 8, 7.5, 9];
+const medias = [10, 9, 8, 7];
 
 const lista = [alunos, medias];
 
-console.log(lista); //Imprime a lista completa de dados dos alunos e alunas
+console.log(`Olá ${lista[0][0]}, você tem média ${lista[1][1]} no curso.`); //Imprime a lista completa de dados dos alunos e alunas
 
-
-function qualAMedia() {
-    let nomeAluno = prompt("Digite seu 1º nome: ");
-    if (alunos.includes(nomeAluno)) {
-        alert(`Olá ${nomeAluno}, você tem média ${medias[0]} no curso.`);
-    } else {
-        alert(`Olá ${nomeAluno}, seu nome não consta na lista de alunos deste curso.`);
-    }
-};
-
-qualAMedia()
+// function qualAMedia() {
+//     let nomeAluno = prompt("Digite seu 1º nome: ");
+//     if (alunos.includes(nomeAluno)) {
+//         alert(`Olá ${nomeAluno}, você tem média ${matricula} no curso.`);
+//     } else {
+//         alert(`Olá ${nomeAluno}, seu nome não consta na lista de alunos deste curso.`);
+//     }
+// };
+// qualAMedia()
