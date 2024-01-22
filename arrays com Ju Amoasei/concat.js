3db96948355958d8f22bbb0e477c70748d733476
roberjunnior <roberjunior.dev@gmail.com>
2024-01-22 01:12:06 -0300

    update - concat

---
+++ arrays com Ju Amoasei/concat.js
@@ -0,0 +1,6 @@
+const sala1 = ['Joao', 'Juliana', 'Ana', 'Caio', 'Lara', 'Marjorie', 'Guilherme', 'Aline', 'Fabiana', 'André'];
+
+const sala2 = ['Carlos', 'Paulo', 'Bia', 'Vivian', 'Isabela', 'Vinícius', 'Renan', 'Renata', 'Daisy', 'Camilo'];
+
+const salas = sala1.concat(sala2);
+console.log(salas);
