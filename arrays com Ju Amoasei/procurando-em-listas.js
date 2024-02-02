c94c2cac9db5ead1be73f6f041f32859ad292ec5
roberjunnior <roberjunior.dev@gmail.com>
2024-02-02 01:28:18 -0300

    update - loops & arrays

---
+++ arrays com Ju Amoasei/procurando-em-listas.js
@@ -0,0 +1,14 @@
+const alunos = ["João", "Juliana", "Caio", "Ana"];
+const medias = [10, 8, 7.5, 9];
+
+const lista = [alunos, medias];
+
+function exibeNomeENota(aluno) {
+  if (lista[0].includes(aluno)) {
+    // condição:  se existir o nome
+    const indice = lista[0].indexOf(aluno);
+    const mediaAluno = lista[1];
+  }
+}
+
+let joao = 23;
