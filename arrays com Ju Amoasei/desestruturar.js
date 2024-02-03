6908a7f73f6a38bef1766b8b86174e11d99f769a
roberjunnior <roberjunior.dev@gmail.com>
2024-02-03 15:49:43 -0300

    update - loops & arrays

--- arrays com Ju Amoasei/desestruturar.js
+++ arrays com Ju Amoasei/desestruturar.js
@@ -5,9 +5,12 @@ const lista = [alunos, medias];
 
 function exibeNomeENota(aluno) {
   if (lista[0].includes(aluno)) {
-    // condição:  se existir o nome do aluno na lista
-    const indice = lista[0].indexOf(aluno);
-    const mediaAluno = lista[1][indice];
+    // apresentando a nova const [alunos, media]
+    // não há necessidade de indicar lista[0] pois sabemos que a posição do vetor é 0.
+    const [alunos, medias] = lista;
+
+    const indice = alunos.indexOf(aluno);
+    const mediaAluno = medias[indice];
     console.log(`${aluno} tem a média ${mediaAluno}.`);
   } else {
     console.log(`${aluno}, seu nome não está na lista.`);
@@ -15,12 +18,3 @@ function exibeNomeENota(aluno) {
 }
 exibeNomeENota("Juliana");
 exibeNomeENota("Ricardo");
-
-// let nome = "Jose";
-// let sobrenome = "Silva";
-
-// function qualSeuNome() {
-//   return `Seu nome é ${nome} ${sobrenome}.`;
-// }
-
-// console.log(qualSeuNome());
---
+++ arrays com Ju Amoasei/i++.js
@@ -0,0 +1,11 @@
+// Exemplo de ++i
+let i = 5;
+console.log(i++); // Saída: 5 (retorna o valor atual de i)
+console.log(i); // Saída: 6 (i foi incrementado após a execução)
+
+// Exemplo de ++i
+let j = 8;
+console.log(++j); // Saída: 9 (j foi incrementado antes de ser usado)
+console.log(j); // Saída: 9 (j já foi incrementado)
+
+//Importante: para estes exemplos usamos os nomes de variáveis padrão i e j, porém o princípio é o mesmo para qualquer nome de variável que você use, por exemplo minhaVar++ ou ++incremento.
