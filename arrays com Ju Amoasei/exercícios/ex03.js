5e3bed42c234106f425f89a2089d16d61bc073d1
roberjunnior <roberjunior.dev@gmail.com>
2024-02-03 15:53:05 -0300

    update - loops & arrays

---
+++ arrays com Ju Amoasei/exercícios/ex03.js
@@ -0,0 +1,15 @@
+let i = 5;
+let valorA = i++;
+let valorB = ++i;
+
+console.log("O valor de valorA é:", valorA);
+console.log("O valor de valorB é:", valorB);
+
+//Identifique a alternativa que descreve corretamente os valores de valorA e valorB após a execução do código:
+
+// valorA é 6 e valorB é 6.
+
+// Alternativa correta
+// valorA é 5 e valorB é 7.
+
+// O operador i++ faz um pós-incremento, o que significa que primeiro atribui o valor atual de i a valorA e depois incrementa 1 em i. Em seguida, o operador ++i incrementa o valor de i e atribui esse valor a valorB.
